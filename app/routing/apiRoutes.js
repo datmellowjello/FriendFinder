1717bfee952bea7d3d9050eb738e873db80a3b33
davidbridges123 <david.bridges@live.com>
2019-10-14 16:22:32 -0600

    updates

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,3 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
 
 // var tableData = require("../data/tableData");
 var friends = require('../data/friends');
@@ -14,11 +9,7 @@ var friends = require('../data/friends');
 // ===============================================================================
 
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
+ 
 
   app.get("/api/friends", function(req, res) {
     res.json(usersArray);
@@ -37,10 +28,7 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
   app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-    // if (tableData.length < 5) {
+
 
     var newFriend = req.body;
 
